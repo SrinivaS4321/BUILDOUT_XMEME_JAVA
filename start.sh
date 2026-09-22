2be3d34555e27fdec018d8fa70292f37bf679f6f
chaitanya <chaitanyasrinivas30@gmail.com>
2026-09-22 16:23:32 +0530

    Fix Vercel container deployment

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 mkdir -p /data/db
-mkdir log
-mongod --fork --logpath log/mongod.log --bind_ip 0.0.0.0
-ps -aux | grep mongod
+mkdir -p /code/log
+
+mongod --fork --logpath /code/log/mongodb.log --bind_ip 127.0.0.1
 
 cd /code/sample-data
+chmod +x load-sample-data.sh
 ./load-sample-data.sh
-cd -
-java -jar /code/build/libs/spring-starter-0.0.1-SNAPSHOT.jar
+cd /code
+
+java -jar /code/build/libs/spring-starter-0.0.1-SNAPSHOT.jar --server.port=${PORT:-8080}
